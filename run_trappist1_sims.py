643170c2cb55363c2053f2e3f9c7ba70355fdf35
Dan Chen <danchen@Dans-MacBook-Pro.local>
2026-04-15 13:31:03 -0500

    Periodic backup

--- run_trappist1_sims.py
+++ run_trappist1_sims.py
@@ -5,6 +5,7 @@ import numpy as np
 import scipy
 import multiprocessing
 import pickle as pkl
+import os
 import trappist1_sim as t1
 from time import time
 from astropy import units as u
@@ -20,7 +21,7 @@ r_earth = u.earthRad.to(u.AU)
 m_earth = u.Mearth.to(u.Msun)
 r_sun = u.Rsun.to(u.AU) 
 
-def generate_params(planet_names):
+def generate_params(planet_names, rng):
     # Nested dict containing params for each planet in sim
     # Randomly generate mass, radius, & semimajor axis values
     planet_params = {f"{planet_name}": t1.generate_params_from_csv(f'TRAPPIST-1_params/TRAPPIST-1_{planet_name}_planet_params.csv', ('a (au)', 'Rp (R⨁)', 'Mp (M⨁)'), random=False) for planet_name in planet_names}
@@ -29,9 +30,9 @@ def generate_params(planet_names):
     # Define planet masses (m)
     m_vals = np.array([planet_params[planet_name]['Mp (M⨁)'] for planet_name in planet_names])
 
-    # m_vals[0] = np.random.uniform(1, 1.5) # (actual observed is 1.37 M_earth) random b mass
-    # m_vals[1] = np.random.uniform(1, 1.5) # (actual observed is 1.31 M_earth) random c mass
-    # m_vals[2] = np.random.uniform(1.3, 1.7) # (actual observed is 0.39 M_earth) random d mass
+    # m_vals[0] = rng.uniform(1, 1.5) # (actual observed is 1.37 M_earth) random b mass
+    # m_vals[1] = rng.uniform(1, 1.5) # (actual observed is 1.31 M_earth) random c mass
+    # m_vals[2] = rng.uniform(1.3, 1.7) # (actual observed is 0.39 M_earth) random d mass
     
     m_vals *= m_earth # convert to Msun
 
@@ -44,22 +45,22 @@ def generate_params(planet_names):
     r_star = stellar_params['R✶ (R⦿)'] * r_sun
 
     # Draw initial ratios from log normal
-    # initial_P_ratios = np.random.lognormal(0.6, 0.2, size=len(planet_names)-1) 
+    # initial_P_ratios = rng.lognormal(0.6, 0.2, size=len(planet_names)-1) 
     #                                     # In Keller, 0.703 & 0.313
                                         
     # # Constant period ratios
     # initial_P_ratios = np.full(len(planet_names)-1, 1.9)
     
     # Uniform random just above 2:1
-    initial_P_ratios = np.random.uniform(2.05, 2.1, size=len(planet_names)-1) 
+    initial_P_ratios = rng.uniform(2.05, 2.1, size=len(planet_names)-1) 
                                         
     # Draw surface density at 1au from log uniform
-    Sigma_1au = scipy.stats.loguniform.rvs(a=50, b=1000, size=1) # in g/cm^2
+    Sigma_1au = scipy.stats.loguniform.rvs(a=50, b=1000, size=1, random_state=rng) # in g/cm^2
                                 # In Keller, 10 & 10000
     Sigma_1au *= AU**2 / Msun # unit conversion for sim
 
     # Draw K-factor from log uniform and solve for h
-    K_factor = scipy.stats.loguniform.rvs(a=10, b=200, size=1)
+    K_factor = scipy.stats.loguniform.rvs(a=10, b=200, size=1, random_state=rng)
                                 # In Keller, 10 & 1000
                                 
     return m_vals, r_vals, m_star, r_star, initial_P_ratios, Sigma_1au, K_factor
@@ -71,12 +72,15 @@ dataset_id = 12
 n_sims = 200
 
 def run_sim(sim_id):
+    # Different rng for each sim
+    rng = np.random.default_rng(seed=sim_id + os.getpid())
+    
     # Set where to save the data
     base_dir = Path.cwd()
     file_path = base_dir.parent / "sim_results" / f"dataset{dataset_id}" / f"sim{sim_id}.h5"
     
     # Get random param values
-    m_vals, r_vals, m_star, r_star, initial_P_ratios, Sigma_1au, K_factor = generate_params(planet_names)
+    m_vals, r_vals, m_star, r_star, initial_P_ratios, Sigma_1au, K_factor = generate_params(planet_names, rng)
     
     # Sim integration!
     outcome = t1.simulate_trappist1(m_vals, r_vals, m_star, r_star, initial_P_ratios, Sigma_1au, K_factor, planet_names, sim_id, file_path)
